963c7a5e078061aaab39532f172d32ef20966adf
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-08-17 13:54:51 -0300

    teste

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+    int N;
+
+    printf("coloque o numero: ");   
+    scanf("%i",&N);
+
+    printf("Numero: %i", N);   
+
+    return 0;
+}
