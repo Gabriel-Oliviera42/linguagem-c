8888828dfefba1c756b47776dbed8caad148c70c
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-08-23 20:05:24 -0300

    ex 2 pronto

---
+++ 23-08-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int I;
+
+    for (I = 0; I <= 20; I += 2) {
+        printf("%i\n", I);
+    }
+
+    return 0;
+}
--- 23-08.1.c
+++ 23-08.1.c
@@ -5,7 +5,7 @@ int main(void)
     int I;
 
     for (I = 0; I = 10; I++) {
-        printf("Ciência da Computação");
+        printf("Ciencia da Computacao\n");
     }
 
     return 0;
