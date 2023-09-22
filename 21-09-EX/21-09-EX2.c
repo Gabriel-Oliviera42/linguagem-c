3c568f1e77be5e37661feea383982f4caa5a78cc
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-09-21 21:06:17 -0300

    .

--- 21-09-EX/21-09-EX2.c
+++ 21-09-EX/21-09-EX2.c
@@ -11,26 +11,26 @@ transposta ( 2 x 3).Exemplo:*/
 #include <stdlib.h>
 
 int main(void) {
-    int Matriz [3][2] = {
-        //C1 C2 
-          {3,7}, //L1
-         {9,85}, //L2
-        {141,22} //L3
+    int Matriz[3][2] = {
+        {3, 7},
+        {-9, 85},
+        {141, 22}
     };
-    int MatrizTransposta [2][3];
-    int j, i;
+    int MatrizTransposta[2][3];
+    int i, j;
 
+    // Calcula a matriz transposta
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 3; j++) {
-            MatrizTransposta [i][j] = 0;
+            MatrizTransposta[i][j] = Matriz[j][i];
         }
     }
 
+    // Imprime a matriz transposta
+    printf("Matriz Transposta:\n");
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 3; j++) {
-            MatrizTransposta  [j][i] = Matriz [i][j];
-            printf("%d ", MatrizTransposta [i][j]);
-
+            printf("%d ", MatrizTransposta[i][j]);
         }
         printf("\n");
     }
