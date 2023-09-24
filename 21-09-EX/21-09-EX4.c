b3d361fa8d67e3a1fdf46f4f1f76c3fdc4228978
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-09-23 23:58:04 -0300

    .

--- 21-09-EX/21-09-EX4.c
+++ 21-09-EX/21-09-EX4.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+#define NUM_CIDADES 5
 
+int main() {
+    char cidades[NUM_CIDADES][50];
+    float distancia[NUM_CIDADES][NUM_CIDADES];
+    float consumo;
+    
+    // Leitura dos nomes das cidades
+    printf("Digite os nomes das cinco cidades diferentes:\n");
+    for (int i = 0; i < NUM_CIDADES; i++) {
+        printf("Cidade %d: ", i + 1);
+        scanf("%s", cidades[i]);
+    }
+    
+    // Inicialização da diagonal principal da matriz de distâncias com 0
+    for (int i = 0; i < NUM_CIDADES; i++) {
+        for (int j = 0; j < NUM_CIDADES; j++) {
+            if (i == j) {
+                distancia[i][j] = 0;
+            } else {
+                printf("Digite a distância entre %s e %s: ", cidades[i], cidades[j]);
+                scanf("%f", &distancia[i][j]);
+            }
+        }
+    }
+    
+    // Leitura do consumo de combustível
+    printf("Digite o consumo de combustível em quilômetros por litro: ");
+    scanf("%f", &consumo);
+    
+    // Cálculo dos percursos que não ultrapassam 250 quilômetros
+    printf("\nPercursos que não ultrapassam 250 quilômetros:\n");
+    for (int i = 0; i < NUM_CIDADES; i++) {
+        for (int j = 0; j < NUM_CIDADES; j++) {
+            if (distancia[i][j] <= 250) {
+                printf("%s -> %s\n", cidades[i], cidades[j]);
+            }
+        }
+    }
+    
+    // Cálculo de todos os percursos e quantidade de combustível necessária
+    printf("\nTodos os percursos e quantidade de combustível necessária:\n");
+    for (int i = 0; i < NUM_CIDADES; i++) {
+        for (int j = 0; j < NUM_CIDADES; j++) {
+            if (i != j) {
+                float combustivel_necessario = distancia[i][j] / consumo;
+                printf("%s -> %s: %.2f litros\n", cidades[i], cidades[j], combustivel_necessario);
+            }
+        }
+    }
+    
     return 0;
 }
