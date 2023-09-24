ced789c15572a93fcc342cbb9fbe50578986ffb2
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-09-24 14:13:18 -0300

    .

--- 21-09-EX/21-09-EX4.c
+++ 21-09-EX/21-09-EX4.c
@@ -1,9 +1,37 @@
+/*
+
+- Faça um programa que receba:
+
+Um vetor com o nome de cinco cidades diferentes.
+
+Uma matriz 5 x 5 com a distância entre as cidades, sendo que na diagonal
+principal deve ser colocada automaticamente distância zero, ou seja, não deve
+ser permitida a digitação.
+
+- Calcule e mostre:
+
+Os percursos que não ultrapassam 250 quilômetros (os percursos são compostos
+pelos nomes das cidades de origem e pelos nomes das cidades de destino);
+
+O consumo de um veículo, ou seja, quantos quilômetros o veículo faz por litro
+de combustível e mostre um relatório com a quantidade de combustível
+necessária para percorrer cada percurso citando o mesmo (nome da cidade de
+origem e nome da cidade de destino);
+
+A maior distância e em que percurso se encontra (nome da cidade de origem e
+nome da cidade de destino).
+
+   0   1   2   3   4    
+0  0  250 100 300 200
+1 250  0  360  75  80
+2 100 360  0  250 800
+3 300  75 250  0  530
+4 200  80 800 530  0
+
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-int main (void) {
-
-
+    
     return 0;
 }
