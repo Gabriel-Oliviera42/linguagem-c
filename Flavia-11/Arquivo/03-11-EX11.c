8c84b763b58493fc2fdf2162e60702208c28fe04
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-03 15:41:38 -0300

    .

--- Flavia-11/Arquivo/03-11-EX11.c
+++ Flavia-11/Arquivo/03-11-EX11.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
-{
+{ 
     int x;
-    x = rename("c:\\exemplo\\teste\\clientes.dat", "c:\\exemplo\\teste\\dados.cli");
+    x = rename("c:\\exemplo\\teste\\clientes.dat", "c:\\atividades\\dados.cli");
     if (x == 0)
         printf("Sucesso na troca de nome");
     else 
