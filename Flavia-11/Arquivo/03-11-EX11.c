fb0123ab363f10c767546025da9731aa27dd6c88
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-05 14:38:06 -0300

    .

--- Flavia-11/Arquivo/03-11-EX11.c
+++ Flavia-11/Arquivo/03-11-EX11.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-int main()
-{ 
-    int x;
-    x = rename("c:\\exemplo\\teste\\clientes.dat", "c:\\atividades\\dados.cli");
-    if (x == 0)
-        printf("Sucesso na troca de nome");
+#include <stdio.h> // Inclui a biblioteca de entrada e saída padrão em C.
+
+int main() { // Função principal do programa.
+    int x; // Declara uma variável inteira.
+    x = rename("c:\\exemplo\\teste\\clientes.dat", "c:\\atividades\\dados.cli"); // Tenta renomear o arquivo.
+
+    if (x == 0) // Verifica se o valor retornado por rename é 0 (o que indica sucesso na troca de nome).
+        printf("Sucesso na troca de nome"); // Imprime uma mensagem de sucesso na troca de nome.
     else 
-        printf("Erro na troca de nome");
-    getchar();
-    return 0;
+        printf("Erro na troca de nome"); // Imprime uma mensagem de erro na troca de nome.
+
+    getchar(); // Aguarda a entrada de um caractere antes de encerrar o programa.
+    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso.
 }
--- Flavia-11/Arquivo/03-11-EX9.c
+++ Flavia-11/Arquivo/03-11-EX9.c
@@ -1,36 +1,48 @@
-#include <stdio.h>
+#include <stdio.h> // Inclui a biblioteca de entrada e saída padrão em C.
 
-int main() {
-    FILE *cli;
-    struct cliente {
+int main() { // Função principal do programa.
+
+    // Declarações de variáveis
+    FILE *cli; // Declara um ponteiro para um arquivo.
+    struct cliente { // Definição da estrutura 'cliente'
         int numero;
         char nome[20];
     };
-    struct cliente c;
+    struct cliente c; // Declara uma estrutura 'cliente' chamada 'c'.
+
+    // Abre o arquivo em modo de adição e leitura.
     cli = fopen("c:\\exemplo\\teste\\clientes.dat", "a+");
-    if (cli == NULL)
-        printf("\nErro na abertura do arquivo");
+
+    if (cli == NULL) // Verifica se houve erro na abertura do arquivo.
+        printf("\nErro na abertura do arquivo"); // Imprime uma mensagem de erro caso não seja possível abrir o arquivo.
     else {
+        // Lê a estrutura 'cliente' do arquivo.
         fread(&c, sizeof(struct cliente), 1, cli);
-        if (ferror(cli))
-            printf("\nErro na leitura");
+
+        if (ferror(cli)) // Verifica se ocorreu algum erro durante a leitura do arquivo.
+            printf("\nErro na leitura"); // Imprime uma mensagem de erro caso ocorra um erro na leitura.
         else {
-            printf("\nLeitura realizada com sucesso");
-            printf("\nNúmero do cliente: %d", c.numero);
-            printf("\nNome do cliente: %s", c.nome);
-            rewind(cli);
-            printf("\nCursor reposicionado no início");
+            printf("\nLeitura realizada com sucesso"); // Imprime uma mensagem de sucesso na leitura.
+            printf("\nNúmero do cliente: %d", c.numero); // Imprime o número do cliente lido.
+            printf("\nNome do cliente: %s", c.nome); // Imprime o nome do cliente lido.
+
+            rewind(cli); // Reposiciona o cursor no início do arquivo.
+
+            printf("\nCursor reposicionado no início"); // Imprime uma mensagem informando que o cursor foi reposicionado.
+
+            // Lê a estrutura 'cliente' do arquivo novamente.
             fread(&c, sizeof(struct cliente), 1, cli);
-            if (ferror(cli))
-                printf("\nErro na leitura");
+
+            if (ferror(cli)) // Verifica se ocorreu algum erro durante a leitura do arquivo.
+                printf("\nErro na leitura"); // Imprime uma mensagem de erro caso ocorra um erro na leitura.
             else {
-                printf("\nLeitura realizada com sucesso");
-                printf("\nNúmero do cliente: %d", c.numero);
-                printf("\nNome do cliente: %s", c.nome);
+                printf("\nLeitura realizada com sucesso"); // Imprime uma mensagem de sucesso na leitura.
+                printf("\nNúmero do cliente: %d", c.numero); // Imprime o número do cliente lido.
+                printf("\nNome do cliente: %s", c.nome); // Imprime o nome do cliente lido.
             }
         }
-        fclose(cli);
+        fclose(cli); // Fecha o arquivo.
     }
-    getchar();
-    return 0;
+    getchar(); // Aguarda a entrada de um caractere antes de encerrar o programa.
+    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso.
 }
