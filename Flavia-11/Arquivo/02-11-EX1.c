fc0953e1b07145ebbdd67b358ce8d32b1f876bd2
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-04 23:57:31 -0300

    .

--- Flavia-11/Arquivo/02-11-EX1.c
+++ Flavia-11/Arquivo/02-11-EX1.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> // Inclui a biblioteca padrão de entrada e saída em C.
 
-int main() {
-    FILE *arq;
-    arq = fopen("arquivo1.dat", "r");
-    if (arq == NULL) {
-        printf("\nOcorreu um erro. O arquivo não foi aberto.");
+int main() {   // Função principal do programa.
+    FILE *arq; // Declaração de um ponteiro para um arquivo.
+    arq = fopen("arquivo1.dat", "r"); // Abre o arquivo "arquivo1.dat" em modo de leitura.
+
+    if (arq == NULL) { // Verifica se ocorreu um erro ao abrir o arquivo.
+        printf("\nOcorreu um erro. O arquivo não foi aberto."); // Imprime uma mensagem de erro.
     } else {
         /* As demais instruções do programa só poderão ser
         executadas se o arquivo foi aberto de forma correta. */
     }
-    // continuação do programa
-    return 0;
+    return 0; // Retorna 0, indicando que o programa foi executado com sucesso.
 }
