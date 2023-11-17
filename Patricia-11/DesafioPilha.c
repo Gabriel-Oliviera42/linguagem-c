508eee8381bf6338ff1786db3a0c69c76d8dc5e7
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-16 21:20:28 -0300

    .

--- Patricia-11/DesafioPilha.c
+++ Patricia-11/DesafioPilha.c
@@ -3,23 +3,23 @@ void inserir() {
 	int continuar;
 	do {
 	cabec();
-printf (“\nColocar livro no topo da pilha  \n”);
-printf(“\nCodigo do livro:     “);
-scanf(“%d”  , &||.codigo);
-printf(“\nTitulo do Livro:   “);
-fflush (stdin);
-gets(||.titulo);
+	printf (“\nColocar livro no topo da pilha  \n”);
+	printf(“\nCodigo do livro:     “);
+	scanf(“%d”  , &||.codigo);
+	printf(“\nTitulo do Livro:   “);
+	fflush (stdin);
+	gets(||.titulo);
 
-// Inserir Livro na pilha
-if (tampilha <30) { /*Se ainda tem vaga na pilha */
-livro[tampilha] = ||;
-tampilha ++;
+	// Inserir Livro na pilha
+	if (tampilha <30) { /*Se ainda tem vaga na pilha */
+	livro[tampilha] = ||;
+	tampilha ++;
 
-printf( “\n\nInserido com Sucesso!! \n\n”);
-}
-else /* Pilha cheia*/
-printf (“\n\nPilha cheia, Livro não foi inserido !! \n\n”);
-printf(“\n Continuar inserindo (1-Sim/2-Não)? “);
-scanf (“%d”, &continuar);
-}while (continuar == 1)// Verificar se gostaria de continuar inserindo livros
+	printf( “\n\nInserido com Sucesso!! \n\n”);
+	}
+	else /* Pilha cheia*/
+		printf (“\n\nPilha cheia, Livro não foi inserido !! \n\n”);
+		printf(“\n Continuar inserindo (1-Sim/2-Não)? “);
+		scanf (“%d”, &continuar);
+	}while (continuar == 1)// Verificar se gostaria de continuar inserindo livros
 }
