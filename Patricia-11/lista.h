8eea42d30f5648e0ed55ed61f330fe5367d049d9
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-29 19:24:40 -0300

    .

--- Patricia-11/lista.h
+++ Patricia-11/lista.h
@@ -1,21 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nome[20];
+    float nota;
+    int turma;
+} Aluno;
 
 typedef struct no {
-    int valor;
+    Aluno aluno;
     struct no *proximo;
     struct no *anterior;
 } No;
 
-void inserir_no_inicio(No **lista, int num) {
+void inserir_no_inicio(No **lista, Aluno aluno) {
     No *novo = malloc(sizeof(No));
 
-    if(novo) {
-        novo -> valor = num;
-        novo -> proximo = *lista;
-        novo -> anterior = NULL;
-        if(*lista) {
-            (*lista) -> anterior = novo;
+    if (novo) {
+        novo->aluno = aluno;
+        novo->proximo = *lista;
+        novo->anterior = NULL;
+        if (*lista) {
+            (*lista)->anterior = novo;
         }
         *lista = novo;
     } else {
@@ -23,46 +30,45 @@ void inserir_no_inicio(No **lista, int num) {
     }
 }
 
-void inserir_no_fim(No **lista, int num) {
+void inserir_no_fim(No **lista, Aluno aluno) {
     No *aux, *novo = malloc(sizeof(No));
 
-    if(novo) {
-        novo -> valor = num;
-        novo -> proximo = NULL;
-        
-        // é o primeiro?
+    if (novo) {
+        novo->aluno = aluno;
+        novo->proximo = NULL;
+
         if (*lista == NULL) {
             *lista = novo;
-            novo -> anterior = NULL;
+            novo->anterior = NULL;
         } else {
             aux = *lista;
-            while (aux -> proximo) {
-                aux = aux -> proximo;
+            while (aux->proximo) {
+                aux = aux->proximo;
             }
-            aux -> proximo = novo;
-            novo -> anterior = aux;
+            aux->proximo = novo;
+            novo->anterior = aux;
         }
     } else {
         printf("Erro ao alocar memoria");
     }
 }
 
-void inserir_no_meio(No **lista, int num, int ant) {
+void inserir_no_meio(No **lista, Aluno aluno, int ant) {
     No *aux, *novo = malloc(sizeof(No));
 
-    if(novo) {
-        novo -> valor = num;
-        // é o primeiro?
+    if (novo) {
+        novo->aluno = aluno;
+
         if (*lista == NULL) {
-            novo -> proximo = NULL;
-            novo -> anterior = NULL;
+            novo->proximo = NULL;
+            novo->anterior = NULL;
             *lista = novo;
         } else {
             aux = *lista;
-            while(aux->valor != ant && aux->proximo)
+            while (aux->aluno.turma != ant && aux->proximo)
                 aux = aux->proximo;
             novo->proximo = aux->proximo;
-            if(aux->proximo) {
+            if (aux->proximo) {
                 aux->proximo->anterior = novo;
             }
             novo->anterior = aux;
@@ -73,29 +79,29 @@ void inserir_no_meio(No **lista, int num, int ant) {
     }
 }
 
-No* remover(No **lista, int num) {
+No* remover(No **lista, int turma) {
     No *aux, *remover = NULL;
 
-    if(*lista) {
-        if((*lista) -> valor == num) {
+    if (*lista) {
+        if ((*lista)->aluno.turma == turma) {
             remover = *lista;
-            *lista = remover -> proximo;
-            if(*lista) {
-                (*lista) -> anterior = NULL;
+            *lista = remover->proximo;
+            if (*lista) {
+                (*lista)->anterior = NULL;
             }
         } else {
             aux = *lista;
-            while (aux -> proximo && aux -> proximo -> valor != num) {
-                aux = aux -> proximo;
+            while (aux->proximo && aux->proximo->aluno.turma != turma) {
+                aux = aux->proximo;
             }
-            if (aux -> proximo) {
-                remover = aux -> proximo;
-                aux -> proximo = remover -> proximo;
-                if(aux -> proximo) {
-                    aux -> proximo -> anterior = aux;
+            if (aux->proximo) {
+                remover = aux->proximo;
+                aux->proximo = remover->proximo;
+                if (aux->proximo) {
+                    aux->proximo->anterior = aux;
                 }
             }
-        } 
+        }
     }
 
     return remover;
@@ -104,16 +110,16 @@ No* remover(No **lista, int num) {
 void imprimir(No *no) {
     printf("\n\tLista: ");
     while (no) {
-        printf("%d ", no -> valor);
-        no = no -> proximo;
+        printf("%s (%.2f) ", no->aluno.nome, no->aluno.nota);
+        no = no->proximo;
     }
     printf("\n\n");
 }
 
-No* ultimo (No **lista) {
+No* ultimo(No **lista) {
     No *aux = *lista;
-    while (aux -> proximo) {
-        aux = aux -> proximo;
+    while (aux->proximo) {
+        aux = aux->proximo;
     }
     return aux;
 }
@@ -121,8 +127,8 @@ No* ultimo (No **lista) {
 void imprimirContrario(No *no) {
     printf("\n\tLista: ");
     while (no) {
-        printf("%d ", no -> valor);
-        no = no -> anterior;
+        printf("%s (%.2f) ", no->aluno.nome, no->aluno.nota);
+        no = no->anterior;
     }
     printf("\n\n");
 }
